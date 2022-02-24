ad4e2ae78abf03e105445b9d359e5538a56a1640
Sean Marjason <smarj1@outlook.com>
2022-02-24 22:48:17 +0000

    Add comments to colour scheme component

--- Source/ColourScheme.h
+++ Source/ColourScheme.h
@@ -15,13 +15,25 @@
 class ColourScheme
 {
 public:
+    /** Base colour for the application, to be used as borders around components */
     static juce::Colour borderColour;
+    
+    /** Base colour for components */
     static juce::Colour backgroundColour;
+    
+    /** Colour for actionable elements of components */
     static juce::Colour secondaryColour;
     
+    /** Primary ascent colour for key features of application */
     static juce::Colour primaryAscent;
+    
+    /** Red secondary ascent colour */
     static juce::Colour redAscent;
+    
+    /** Green secondary ascent colour */
     static juce::Colour greenAscent;
+    
+    /** Grey secondary ascent colour */
     static juce::Colour greyAscent;
     
 private:
