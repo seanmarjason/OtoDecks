173e1e4adc91af905df82665f1ed23c7c0b67af8
Sean Marjason <smarj1@outlook.com>
2022-02-24 22:17:58 +0000

    Add comments to Looping Component

--- Source/LoopingComponent.h
+++ Source/LoopingComponent.h
@@ -13,33 +13,37 @@
 #include <JuceHeader.h>
 #include "DJAudioPlayer.h"
 
-//==============================================================================
-/*
-*/
 class LoopingComponent  :   public juce::Component,
                             public juce::Button::Listener
 {
-public:
-    LoopingComponent(DJAudioPlayer* player);
-    
-    ~LoopingComponent() override;
+    public:
+        LoopingComponent(DJAudioPlayer* player);
+        ~LoopingComponent() override;
 
-    void paint (juce::Graphics&) override;
-    void resized() override;
+        /** Callback called by the operating system to render component on screen
+         * @param g graphics context used for drawing a component or image
+        */
+        void paint (juce::Graphics& g) override;
     
-    /** Implement juce::Button::Listener */
-    void buttonClicked (juce::Button *) override;
+        /** Callback called when this component's size has been changed
+         */
+        void resized() override;
+        
+        /** Callback when the button is clicked
+         * @param button pointer to the button that was clicked
+         */
+        void buttonClicked (juce::Button* button) override;
 
-private:
-    juce::TextButton loop2{"-2s"};
-    juce::TextButton loop4{"-4s"};
-    juce::TextButton loop8{"-8s"};
-    juce::TextButton loopManual{"M"};
-    
-    DJAudioPlayer* player;
-    
-    double manualLoopStart;
-    double manualLoopEnd;
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LoopingComponent)
+    private:
+        juce::TextButton loop2{"-2s"};
+        juce::TextButton loop4{"-4s"};
+        juce::TextButton loop8{"-8s"};
+        juce::TextButton loopManual{"M"};
+        
+        DJAudioPlayer* player;
+        
+        double manualLoopStart;
+        double manualLoopEnd;
+        
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LoopingComponent)
 };
